8fd44cf78c9d60c27b876546b2b9ce32c63f9e1a
dannielskkynoff <zipcrafty@gmail.com>
2025-01-04 20:18:02 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app-compat.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth-compat.js";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCjDxtzK9ELCyxLNgmd0ZYDhB1o-bPLWr8",
-  authDomain: "nuxpro-gestao-de-entregas.firebaseapp.com",
-  projectId: "nuxpro-gestao-de-entregas",
-  storageBucket: "nuxpro-gestao-de-entregas.firebasestorage.app",
-  messagingSenderId: "483551326765",
-  appId: "1:483551326765:web:e77ef1e29d69f58efa7ea4",
-  measurementId: "G-FVR4EXZR93"
+    apiKey: "AIzaSyCjDxtzK9ELCyxLNgmd0ZYDhB1o-bPLWr8",
+    authDomain: "nuxpro-gestao-de-entregas.firebaseapp.com",
+    projectId: "nuxpro-gestao-de-entregas",
+    storageBucket: "nuxpro-gestao-de-entregas.firebasestorage.app",
+    messagingSenderId: "483551326765",
+    appId: "1:483551326765:web:e77ef1e29d69f58efa7ea4",
+    measurementId: "G-FVR4EXZR93"
 };
 
-export default firebaseConfig;
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
