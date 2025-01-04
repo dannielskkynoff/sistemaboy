6aad02c6d52d922cd5ffdcfc8530f74a4a3fb456
dannielskkynoff <zipcrafty@gmail.com>
2025-01-04 20:48:47 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,21 @@
-import * as firebase from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app-compat.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyCjDxtzK9ELCyxLNgmd0ZYDhB1o-bPLWr8",
-    authDomain: "nuxpro-gestao-de-entregas.firebaseapp.com",
-    projectId: "nuxpro-gestao-de-entregas",
-    storageBucket: "nuxpro-gestao-de-entregas.firebasestorage.app",
-    messagingSenderId: "483551326765",
-    appId: "1:483551326765:web:e77ef1e29d69f58efa7ea4",
-    measurementId: "G-FVR4EXZR93"
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCjDxtzK9ELCyxLNgmd0ZYDhB1o-bPLWr8",
+  authDomain: "nuxpro-gestao-de-entregas.firebaseapp.com",
+  projectId: "nuxpro-gestao-de-entregas",
+  storageBucket: "nuxpro-gestao-de-entregas.firebasestorage.app",
+  messagingSenderId: "483551326765",
+  appId: "1:483551326765:web:c208a1f41e93350bfa7ea4",
+  measurementId: "G-WT45ZZDVDV"
 };
 
-const app = firebase.initializeApp(firebaseConfig); // Acesso correto
-
-export { app };
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
